b60e57767fe7fb8875cb78dc3e976967d336d755
Grigori Fursin <gfursin@frennes>
2019-02-13 16:23:05 +0100

    added program pipeline (front-end) for runner

---
+++ program/sysml19-aggregathor/ck_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ${CK_ENV_TOOL_SYSML19_AGGREGATHOR}
+
+${CK_ENV_COMPILER_PYTHON_FILE} runner.py \
+ --server '${SERVER}' \
+ --ps-job-name ${PS_JOB_NAME} \
+ --wk-job-name ${WK_JOB_NAME} \
+ --ev-job-name ${EV_JOB_NAME} \
+ --experiment "${EXPERIMENT}" \
+ --learning-rate-args "${LEARNING_RATE_ARGS}" \
+ --aggregator "${AGGREGATOR}" \
+ --nb-workers ${NB_WORKERS} \
+ --max-step ${MAX_STEP} \
+ --evaluation-period ${EVALUATION_PERIOD} \
+ --checkpoint-period ${CHECKPOINT_PERIOD} \
+ --summary-period ${SUMMARY_PERIOD} \
+ --evaluation-delta ${EVALUATION_DELTA} \
+ --checkpoint-delta ${CHECKPOINT_DELTA} \
+ --summary-delta ${SUMMARY_DELTA} \
+ --reuse-gpu \
+ --no-wait
